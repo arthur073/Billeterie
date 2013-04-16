305233058a9728fbc432e12b9a1d5ad56c46a108
Michel Kuhm <Michel.Kuhm@ensimag.imag.fr>
2013-04-16 11:01:47 +0200

    modifs mineures

--- src/main/java/controleur/RepresentationsControleur.java
+++ src/main/java/controleur/RepresentationsControleur.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "RepresentationsControleur", urlPatterns = {"/RepresentationsControleur"})
 public class RepresentationsControleur extends HttpServlet {
     
+    @Override
     public void doGet(HttpServletRequest request,
 			  HttpServletResponse response)
             throws IOException, ServletException {
