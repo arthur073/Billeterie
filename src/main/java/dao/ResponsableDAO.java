f59125150ef9fc2a85c250811fcd2075fc08a601
Jany Belluz <Jany.Belluz@ensimag.imag.fr>
2013-05-03 20:47:57 +0200

    bouté ScriptRunner hors du modèle

--- src/main/java/dao/ResponsableDAO.java
+++ src/main/java/dao/ResponsableDAO.java
@@ -16,7 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
-import modele.ScriptRunner;
+import outils.ScriptRunner;
 
 /**
  *
--- src/main/java/outils/ScriptRunner.java
+++ src/main/java/outils/ScriptRunner.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package modele;
+package outils;
 
 /*
  * Slightly modified version of the com.ibatis.common.jdbc.ScriptRunner class
